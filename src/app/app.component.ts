ca432c6a2cd0ebc0fad380f6da6e7d10170d835e
dev-protim <roypranto445@gmail.com>
2024-05-18 23:20:51 +0200

    Initialized tailwind.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/component/header/header.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, NgbModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { JobListComponent } from './features/pages/job-list/job-list.component';
+import { HomepageComponent } from './features/pages/homepage/homepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomepageComponent
+    }
+];
