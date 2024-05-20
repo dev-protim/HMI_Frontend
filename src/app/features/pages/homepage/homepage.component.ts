cbd1d50e6cb4a332b7afc11bd47b7497435d887f
dev-protim <roypranto445@gmail.com>
2024-05-20 22:17:05 +0200

    Started job card design.

--- src/app/features/pages/homepage/homepage.component.ts
+++ src/app/features/pages/homepage/homepage.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../../core/component/header/header.component';
+import { JobCardComponent } from '../../../core/component/job-card/job-card.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, JobCardComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
